65a2fe01dbfc5e1eb234cf6fd528aebbccc27026
James Fitt <91226933+Dragonshifty@users.noreply.github.com>
2023-05-03 15:41:53 +0100

    Add files via upload

--- Lazoor/EnemySpawner.cs
+++ Lazoor/EnemySpawner.cs
@@ -7,6 +7,7 @@ public class EnemySpawner : MonoBehaviour
     WaveConfigSO currentWave;
     [SerializeField] List<WaveConfigSO> waveConfigs;
     [SerializeField] float TimeBetweenWaves = 0f;
+    [SerializeField] bool isLooping = true;
     void Start()
     {
         StartCoroutine(SpawnEnemyWaves());
@@ -19,19 +20,23 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnEnemyWaves()
     {
-        foreach (WaveConfigSO wave in waveConfigs)
+
+        do 
         {
+        foreach (WaveConfigSO wave in waveConfigs)
+            {
             currentWave = wave;
             for (int i = 0; i < currentWave.GetEnemyCount();i++)
-            {
-                Instantiate(currentWave.GetEnemyPrefab(i), 
-                            currentWave.GetStartingWaypoint().position,
-                            Quaternion.identity,
-                            transform);
-                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
-            }  
+                {
+                    Instantiate(currentWave.GetEnemyPrefab(i), 
+                                currentWave.GetStartingWaypoint().position,
+                                Quaternion.Euler(0, 0, 180),
+                                transform);
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }  
             yield return new WaitForSeconds(TimeBetweenWaves);
-        }
+            }
+        } while (isLooping);
         
     }
 
